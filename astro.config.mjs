f60444656f3eaa03994460471066580c3dcd1675
AmeMeida <ame.pistache@gmail.com>
2022-12-27 17:02:49 -0300

    purgecss finalmente funcionando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import purgecss from 'astro-purgecss';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [purgecss()]
+});
