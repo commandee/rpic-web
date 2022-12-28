dc454d497e9eadb5e3b40ba807a54471d36fbac8
AmeMeida <ame.pistache@gmail.com>
2022-12-27 22:21:49 -0300

    omg svelte hiii

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [purgecss()]
+  integrations: [purgecss(), svelte()]
 });
