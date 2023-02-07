9cbf0a19cab9f821e1c32cf17e2aee4618a58248
AmeMeida <ame.pistache@gmail.com>
2023-02-07 00:49:04 -0300

    componentes de settings isolados. husky com problemas

---
+++ src/components/settings/SettingField.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let legend: string;
+  export let buttonLegend: string = legend;
+  export let buttonIcon: string | null = null;
+</script>
+
+<button>
+  {#if buttonIcon}
+    <img src={buttonIcon} alt="" />
+  {/if}
+
+  {buttonLegend}
+</button>
+
+<fieldset>
+  <legend>{legend}</legend>
+</fieldset>
+
+<style lang="scss">
+</style>
